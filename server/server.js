d0b2766ccac16b29e385ef68d71fd90ae7104916
Fayvor George <phavorsparks@gmail.com>
2018-11-22 21:27:29 +0100

    connect app to the database

--- server/server.js
+++ server/server.js
@@ -6,6 +6,19 @@ const bodyParser = require('body-parser')
 
 const app = express()
 const port = process.env.PORT
+const uri = process.env.MONGODB_URI
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: true
+})
+  .then(() => console.log(`connected to database: ${uri}`))
+  .catch(err => console.log('sorry we could not connect to the database...'))
+
+app.get('/', (r, s) => {
+  s.send('Welcome to the National Central Database System...')
+})
 
 app.listen(port, () => console.log(`server running on port: ${port}`))
 
