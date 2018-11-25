309b4d256905025429288eba513d5e26fcdea019
Fayvor George <phavorsparks@gmail.com>
2018-11-25 23:33:19 +0100

    write custom middleware for cors

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const path = require('path')
-const cors = require('cors')
+// const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT
@@ -12,9 +12,23 @@ const uri = process.env.MONGODB_URI
 
 const servePublic = path.join(__dirname, '../public')
 
-app.use(cors({
-  origin: 'http://localhost:3000/users/register'
-}))
+// app.use(cors({
+//   origin: 'http://localhost:3000/users/register'
+// }))
+
+// CORS middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Method", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Authorization, Accept, X-Requested-With, x-auth-token");
+  // res.header("Acces-Control-Expose-Headers", "x-auth-token")
+
+  if (req.method === "OPTIONS") {
+    return res.end();
+  }
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.static(servePublic))
