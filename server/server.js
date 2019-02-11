c906d3b6d72496f8174f580c971869249d73ca89
Favour George <phavorsparks@gmail.com>
2019-02-11 23:09:10 +0100

    setup ejs and convert partials to ejs files

--- server/server.js
+++ server/server.js
@@ -10,29 +10,30 @@ const app = express()
 const port = process.env.PORT
 const uri = process.env.MONGODB_URI
 
-const servePublic = path.join(__dirname, '../public')
 
 // app.use(cors({
-//   origin: 'http://localhost:3000/users/register'
-// }))
-
-// CORS middleware
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Method", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Authorization, Accept, X-Requested-With, x-auth-token");
-  // res.header("Acces-Control-Expose-Headers", "x-auth-token")
-
-  if (req.method === "OPTIONS") {
-    return res.end();
-  }
-  next();
-});
+  //   origin: 'http://localhost:3000/users/register'
+  // }))
+  
+  // CORS middleware
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Method", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Authorization, Accept, X-Requested-With, x-auth-token");
+    // res.header("Acces-Control-Expose-Headers", "x-auth-token")
+    
+    if (req.method === "OPTIONS") {
+      return res.end();
+    }
+    next();
+  });
+  
+const servePublic = path.join(__dirname, '../public')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(express.static(servePublic))
-app.set('view engine', 'pug')
+app.set('view engine', 'ejs')
 
 const userRoutes = require('./routes/users')
 const appRoutes = require('./routes/appRoute')
